b68ee44c6d12ab86c6b345a7dbe653602b56c964
jaydeep-vc <145459108+jaydeep-vc@users.noreply.github.com>
2023-10-06 17:34:00 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Welcome to Flask Application!"
+    return "WELCOME TO PYTHON GALAXY!"
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
